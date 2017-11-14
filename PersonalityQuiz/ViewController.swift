278fecb54d4167a719641c8a56f09170e5c87c1d
Rob Dekker <rob.dekker@student.uva.nl>
2017-11-14 16:44:08 +0100

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Rob Dekker on 14-11-17.
+//  Copyright © 2017 Rob Dekker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
